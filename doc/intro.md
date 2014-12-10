aec5beef1dcb553cbdad9101e7f974f8cef8ab44
Junjie Mars <junjiemars@gmail.com>
2014-12-10 09:22:02 +0800

    init commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to ticker.git
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/ticker/git_test.clj
@@ -0,0 +1,7 @@
+(ns ticker.git-test
+  (:require [clojure.test :refer :all]
+            [ticker.git :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
