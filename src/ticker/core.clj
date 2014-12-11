ea6e629552d2f46718c7005c95f25a0269e4851a
Junjie Mars <junjiemars@gmail.com>
2014-12-11 15:31:56 +0800

    it's works

--- src/ticker/core.clj
+++ src/ticker/core.clj
@@ -1,70 +1,77 @@
 (ns ticker.core
-  ;;(:import (java.time LocalDateTime))
-  ;;(:import (java.util TimerTask Timer))
-  (:import (java.util.concurrent Executors))
-  (:import (java.util.concurrent TimeUnit))
-  (:use [korma.db])
-  (:use [korma.core])
-  (:require [taoensso.carmine :as car :refer (wcar)])
-  ;;(:require [clojurewerkz.quartzite.scheduler :as qs])
-  ;;(:require [clojurewerkz.quartzite.triggers :as qt])
-  (:gen-class))
+;;(:import (java.time LocalDateTime))
+;;(:import (java.util TimerTask Timer))
+(:import (java.util.concurrent Executors))
+(:import (java.util.concurrent TimeUnit))
+(:import (java.util.concurrent ScheduledExecutorService))
+(:import (java.util.concurrent ScheduledFuture))
+(:import (java.sql SQLException))
+(:use [korma.db])
+(:use [korma.core])
+(:require [taoensso.carmine :as car :refer (wcar)])
+;;(:require [clojurewerkz.quartzite.scheduler :as qs])
+;;(:require [clojurewerkz.quartzite.triggers :as qt])
+(:gen-class))
 
 (declare ora
-         t-tick-values
-         t-tick-nodes
-         redis-connection
-         running)
+       tick-values
+       tick-nodes
+       redis-connection
+       running)
 
 ;;(defn now [] (LocalDateTime/now))
 (def running (ref true))
 
 (def oracle-db {:classname "oracle.jdbc.driver.OracleDriver"
-         :subprotocol "oracle"
-         :subname "thin:@//localhost:1521/XE"
-         :user "xws"
-         :password "xws"})
+       :subprotocol "oracle"
+       :subname "thin:@//localhost:1521/XE"
+       :user "xws"
+       :password "xws"})
 
 (def redis-connect {:pool
-                    {:host "localhost"
-                     :port 6379
-                     :db 1}})
+                  {:host "localhost"
+                   :port 6379
+                   :db 1}})
 (defmacro redis*
-  [& body]
-  `(car/wcar redis-connection  ~@body))
+[& body]
+`(car/wcar redis-connection  ~@body))
 
 (defdb ora oracle-db)
 
-(defentity t-tick-values
-  (table :T_1319_SKILL_NUMBER)
-  (pk :F_OPER_TIME)
-  (entity-fields :F_OPER_TIME
-                 :F_SKILL_NUMBER)
-  ;;(modifier "SYSDATE")
-  )
+(defentity tick-values
+(table :T_1319_SKILL_NUMBER)
+(pk :F_OPER_TIME)
+(entity-fields :F_OPER_TIME
+               :F_SKILL_NUMBER)
+;;(modifier "SYSDATE")
+)
 
-(defentity t-tick-nodes
-  (table :T_1319_SKILLINFO)
-  (entity-fields :F_START_NUM
-                 :F_LEFT_NUM))
+(defentity tick-nodes
+(table :T_1319_SKILLINFO)
+(entity-fields :F_START_NUM
+               :F_LEFT_NUM))
 
-(defn select-t-tick-values
-  []
-  (select t-tick-values
-          (fields [:F_OPER_TIME :F_SKILL_NUMBER])
-          (where (= :F_OPER_TIME
-                    (sqlfn T_NOW)))))
+(defn select-tick-values
+[]
+(try (select tick-values
+              (fields [:F_OPER_TIME :F_SKILL_NUMBER])
+              (where (= :F_OPER_TIME
+                        (sqlfn T_NOW))))
+     (catch SQLException e
+       (println e))))
 
-(defn select-t-tick-nodes
-  [c]
-  (let [s (select t-tick-nodes
-                  (where (> :F_LEFT_NUM 0))
-                  (order :F_START_NUM :ASC))]
-    (when-not (nil? s)
-      (let [n (long (:F_START_NUM (first s)))]
-        (if (<= c n)
-          c
-          n)))))
+(defn select-tick-nodes
+[c]
+(try (let [s (select tick-nodes
+                     (where (> :F_LEFT_NUM 0))
+                     (order :F_START_NUM :ASC))]
+       (when-not (empty? s)
+         (let [n (long (:F_START_NUM (first s)))]
+           (if (<= c n)
+             c
+               n))))
+       (catch SQLException e
+         (println e))))
 
 (defn renew-redis-tick
   "!todo: 
@@ -72,14 +79,16 @@
   if v <= previous then do nothing;
   2). if redis wrong then renew sms notification."
   [v]
-  (redis* (car/set "skill_num_1319" v)))
+  (try (redis* (car/set "skill_num_1319" v))
+       (catch Exception e
+         (println e))))
 
-(defn check-t-tick
+(defn check-tick
   []
-  (let [s (select-t-tick-values)]
+  (let [s (select-tick-values)]
     (when-not (empty? s)
       (let [v (long (:F_SKILL_NUMBER (first s)))
-            n (select-t-tick-nodes v)]
+            n (select-tick-nodes v)]
         ;(println v)
         (println n)
         (renew-redis-tick n)
@@ -87,9 +96,11 @@
 
 (defn ticker
   []
-  (.scheduleAtFixedRate (Executors/newScheduledThreadPool 1)
-                        (check-t-tick)
-                        0 5 TimeUnit/SECONDS))
+  (.scheduleWithFixedDelay
+   (Executors/newScheduledThreadPool 1)
+   check-tick
+   0 5 TimeUnit/SECONDS))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
